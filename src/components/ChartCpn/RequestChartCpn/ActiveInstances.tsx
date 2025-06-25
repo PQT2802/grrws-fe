8c3baf72a32cefce90c15c2d89f0a849c0a652b2
DuckAnt <nguduckant@gmail.com>
2025-06-25 19:55:05 +0700

    minor change

--- src/components/ChartCpn/RequestChartCpn/ActiveInstances.tsx
+++ src/components/ChartCpn/RequestChartCpn/ActiveInstances.tsx
@@ -90,12 +90,17 @@ export default function ActiveInstances() {
 
   const formatDate = (dateString: string) => {
     const date = new Date(dateString);
-    return date.toLocaleDateString('vi-VN', {
+    
+    // Add 7 hours for GMT+7 (Vietnam timezone)
+    const vietnamTime = new Date(date.getTime() + (7 * 60 * 60 * 1000));
+    
+    return vietnamTime.toLocaleDateString('vi-VN', {
       year: 'numeric',
       month: '2-digit',
       day: '2-digit',
       hour: '2-digit',
-      minute: '2-digit'
+      minute: '2-digit',
+      timeZone: 'Asia/Ho_Chi_Minh'
     });
   };
 
